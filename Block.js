1d1e83479dd51c2594938f6baa5af34c6d6229d1
vishaksathishkumar <74096175+vishaksathishkumar@users.noreply.github.com>
2021-07-31 09:30:01 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -15,15 +15,16 @@ class Block{
       if(this.body.speed>3){
        // World.remove(world,this.body)
        push();
-        this.visiblity-=5;
-        tint(0,this.visiblity);
-        image(this.image,this.body.position.x,this.body.position.y,50,50);
+        this.visiblity=-5;
+        tint(255,this.visiblity);
+        image(this.image,this.body.position.x,this.body.position.y,30,40);
         pop();
         }
         else{
-          var pos= this.body.position;
-          imageMode(CENTER);
-          image(this.image,pos.x,pos.y,this.width, this.height);
+        var pos= this.body.position;
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.width, this.height);
         }
+        
     }
 }
